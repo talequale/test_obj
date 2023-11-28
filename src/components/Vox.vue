b6394df6f1356d35932441776cea4132520f51a1
talequale <antonova.julia@gmail.com>
2023-11-28 18:51:56 +0400

    initial commit

---
+++ src/components/Vox.vue
@@ -0,0 +1,19 @@
+<script setup>
+/*defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})*/
+</script>
+
+<template>
+  <div class="object">
+    <h1 class="green"></h1>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
